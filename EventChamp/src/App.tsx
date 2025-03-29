e37fcf7687d5d110875de02f72871a6df6b2bbd5
sami0ahmed <sami872344@gmail.com>
2025-03-29 12:57:19 -0400

    Cleaned up src

--- EventChamp/src/App.tsx
+++ EventChamp/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import Navbar from './components/Navbar'
 import {EventProvider} from './context/eventContext'
 import EventPage from './pages/eventPage'
